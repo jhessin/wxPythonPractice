c8b2d5b1eec26b6213cd5dca68d88d887b5feffb
Jim Hessin <jhessin@gmail.com>
2025-03-09 09:17:40 -0600

    Sun Mar  9 09:17:42 2025

---
+++ customwidgets/burning.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+
+"""
+ZetCode wxPython tutorial
+
+This program creates a Burning widget.
+
+author: Jan Bodnar
+website: zetcode.com
+last edited: May 2018
+"""
+
+import wx
+from wx import Slider
+
+
+class Burning(wx.Panel):
+    cw: int
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, size=(-1, 30), style=wx.SUNKEN_BORDER)
+
+        self.parent = parent
+        self.font = wx.Font(9, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL,
+                            wx.FONTWEIGHT_NORMAL, False, 'Courier 10 Pitch')
+
+        self.Bind(wx.EVT_PAINT, self.on_paint)
+        self.Bind(wx.EVT_SIZE, self.on_size)
+
+    def on_paint(self, e):
+
+        num = range(75, 700, 75)
+        dc = wx.PaintDC(self)
+        dc.SetFont(self.font)
+        w, h = self.GetSize()
+
+        self.cw = self.parent.GetParent().cw
+
+        step = int(round(w / 10.0))
+
+        j = 0
+
+        till = int(round(w / 750.0) * self.cw)
+        full = int(round(w / 750.0) * 700)
+
+        if self.cw >= 700:
+
+            dc.SetPen(wx.Pen('#FFFFB8'))
+            dc.SetBrush(wx.Brush('#FFFFB8'))
+            dc.DrawRectangle(0, 0, full, 30)
+            dc.SetPen(wx.Pen('#ffafaf'))
+            dc.SetBrush(wx.Brush('#ffafaf'))
+            dc.DrawRectangle(full, 0, till - full, 30)
+        else:
+
+            dc.SetPen(wx.Pen('#FFFFB8'))
+            dc.SetBrush(wx.Brush('#FFFFB8'))
+            dc.DrawRectangle(0, 0, till, 30)
+
+        dc.SetPen(wx.Pen('#5C5142'))
+
+        for i in range(step, 10 * step, step):
+            dc.DrawLine(i, 0, i, 6)
+            width, height = dc.GetTextExtent(str(num[j]))
+            dc.DrawText(str(num[j]), i - int(round(width / 2)), 8)
+            j = j + 1
+
+    def on_size(self, e):
+
+        self.Refresh()
+
+
+class Example(wx.Frame):
+
+    wid: Burning
+    sld: wx.Slider
+    cw: int
+
+    def __init__(self, *args, **kwargs):
+        super(Example, self).__init__(*args, **kwargs)
+
+        self.init_ui()
+
+    def init_ui(self):
+        self.cw = 75
+
+        panel = wx.Panel(self)
+        center_panel = wx.Panel(panel)
+
+        self.sld = wx.Slider(center_panel, value=75, maxValue=750, size=(200, -1),
+                             style=wx.SL_LABELS)
+
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        hbox3 = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.wid = Burning(panel)
+        hbox.Add(self.wid, 1, wx.EXPAND)
+
+        hbox2.Add(center_panel, 1, wx.EXPAND)
+        hbox3.Add(self.sld, 0, wx.LEFT | wx.TOP, 35)
+
+        center_panel.SetSizer(hbox3)
+
+        vbox.Add(hbox2, 1, wx.EXPAND)
+        vbox.Add(hbox, 0, wx.EXPAND)
+
+        self.Bind(wx.EVT_SCROLL, self.on_scroll)
+
+        panel.SetSizer(vbox)
+
+        self.sld.SetFocus()
+
+        self.SetTitle("Burning widget")
+        self.Centre()
+
+    def on_scroll(self, e):
+        self.cw = self.sld.GetValue()
+        self.wid.Refresh()
+
+
+def main():
+    app = wx.App()
+    ex = Example(None)
+    ex.Show()
+    app.MainLoop()
+
+
+if __name__ == '__main__':
+    main()
